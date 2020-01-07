0f20a975329c76d6dccd5d6b853e96da5e345782
Nelson <nelsontecti@gmail.com>
2020-01-07 05:28:39 -0300

    first commit

---
+++ lib/repositories/account.repository.dart
@@ -0,0 +1,19 @@
+import 'package:dio/dio.dart';
+import '../models/authenticate-user.model.dart';
+import '../models/create-user.model.dart';
+import '../models/user.model.dart';
+import '../settings.dart';
+
+class AccountRepository {
+  Future<UserModel> authenticate(AuthenticateModel model) async {
+    var url = "${Settings.apiUrl}v1/account/login";
+    Response response = await Dio().post(url, data: model);
+    return UserModel.fromJson(response.data);
+  }
+
+  Future<UserModel> create(CreateUserModel model) async {
+    var url = "${Settings.apiUrl}v1/account";
+    Response response = await Dio().post(url, data: model);
+    return UserModel.fromJson(response.data);
+  }
+}
